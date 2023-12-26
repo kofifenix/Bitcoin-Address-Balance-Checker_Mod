26124a66fd03aeec848b4d2490a24ca5d74e4cdb
Michael Ampofo <asamoahmike@gmail.com>
2023-12-26 15:48:08 +0000

    Create balancechecker_mod_py3.py
    For Python 3.X

---
+++ balancechecker_mod_py3.py
@@ -0,0 +1,37 @@
+import blockcypher
+from moneywagon import AddressBalance
+
+def balance(addr):
+    try:
+        total = blockcypher.get_total_balance(addr)
+        return total
+    except:
+        total = AddressBalance().action('btc', addr)
+        return total
+
+def process_addresses(input_file, output_file):
+    with open(input_file, 'r') as file:
+        addresses = file.read().splitlines()
+
+    valid_addresses = []
+
+    for addr in addresses:
+        addr_balance = balance(addr)
+        print(f'Address: {addr}, Balance: {addr_balance}')
+
+        if addr_balance > 0:
+            valid_addresses.append(addr)
+
+    with open(output_file, 'w') as file:
+        for valid_addr in valid_addresses:
+            file.write(valid_addr + '\n')
+
+def main():
+    input_file = input("Enter the path to the input file with Bitcoin addresses: ")
+    output_file = input("Enter the path to the output file for valid addresses: ")
+
+    process_addresses(input_file, output_file)
+    print("Script execution complete.")
+
+if __name__ == '__main__':
+    main()
